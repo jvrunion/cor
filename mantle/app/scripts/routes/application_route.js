667fe2244f75d90cbe083d7e6218418b453c7395
Ninja <jvrunion@gmail.com>
2014-01-14 17:05:57 -0500

    left panel hover states

--- mantle/app/scripts/routes/application_route.js
+++ mantle/app/scripts/routes/application_route.js
@@ -2,6 +2,6 @@ Core.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
     model: function () {
-        return ['Front End Matters', 'Modern Browsers', 'Data Visualization', 'Mobile First', 'JS MVC', 'WICAGG & Standards'];
+        return ['Front End Matters', 'Modern Browsers', 'Data Visualization', 'Mobile First', 'JS MVC', '(WCAG) 2.0'];
     } 
 });
