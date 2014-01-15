cc8ccf060867476317ea366b28826de48a18ba09
Ninja <jvrunion@gmail.com>
2014-01-15 10:10:15 -0500

    added docking and docking logic

--- mantle/app/scripts/routes/application_route.js
+++ mantle/app/scripts/routes/application_route.js
@@ -1,7 +1,7 @@
 Core.ApplicationRoute = Ember.Route.extend({
     // admittedly, this should be in IndexRoute and not in the
     // top level ApplicationRoute; we're in transition... :-)
-    model: function () {
+    model: function() {
         return ['Front End Matters', 'Modern Browsers', 'Data Visualization', 'Mobile First', 'JS MVC', '(WCAG) 2.0'];
-    } 
+    }
 });
