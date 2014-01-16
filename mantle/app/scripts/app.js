48c1489de00f2f11b558a05f9292d111296b0da8
Ninja <jvrunion@gmail.com>
2014-01-16 13:24:03 -0500

    proper placement of jquery actions

--- mantle/app/scripts/app.js
+++ mantle/app/scripts/app.js
@@ -1,8 +1,14 @@
 var Core = window.Core = Ember.Application.create();
 
-/* Order and include as you please. */
 require('scripts/controllers/*');
 require('scripts/store');
 require('scripts/models/*');
 require('scripts/routes/*');
 require('scripts/views/*');
+
+Core.ApplicationView = Ember.View.extend({
+    didInsertElement: function() {
+        var panelTip = ('.generated-left-panel');
+        this.$(panelTip).tooltip();
+    }
+});
