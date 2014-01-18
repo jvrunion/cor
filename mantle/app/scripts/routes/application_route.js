d96278be65b14661f46f4652c231bd1c701f2962
Ninja <jvrunion@gmail.com>
2014-01-17 22:40:54 -0500

    branding changes, updates

--- mantle/app/scripts/routes/application_route.js
+++ mantle/app/scripts/routes/application_route.js
@@ -1,12 +1,12 @@
 Core.ApplicationRoute = Ember.Route.extend({
     model: function() {
         return [
-            '<a href="#" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="All about The Front End">Front End Matters</a>',
-            '<a href="#matters" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="What is all the fuss about?">Modern Browsers</a>',
-            '<a href="#data" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Info-Graphs and Charts have taken over!">Data Visualization</a>',
-            '<a href="#mobile" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="You mean there are other devices besides my iPad?">Mobile First</a>',
-            '<a href="#mvc" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Backend you have met your match!">JS MVC</a>',
-            '<a href="#wcag" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Standards are what seperate success and failure">(WCAG) 2.0</a>'
+            '<a href="#" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="All About The Front End">Front End Matters</a>',
+            '<a href="#matters" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="What Is All This Modern Browser Talk About?">Modern Browsers</a>',
+            '<a href="#data" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Infographics And Charts Have Taken Over!">Big Data Visualization</a>',
+            '<a href="#mobile" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="You Mean There Is Another Device Out There Besides My iPad?">Mobile First Design</a>',
+            '<a href="#mvc" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Backend...You Sir Have Met Your Match!">Front End MVC</a>',
+            '<a href="#wcag" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Standards Are What Seperate The Successfull From The Failures">(WCAG) 2.0</a>'
         ];
     }
 });
