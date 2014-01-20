0972df655415849f336460508f47531aeda17845
Ninja <jvrunion@gmail.com>
2014-01-19 22:10:02 -0500

    adding left panel nav integration

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -1,8 +1,8 @@
-Core.ApplicationRoute = Ember.Route.extend({
+Cor.ApplicationRoute = Ember.Route.extend({
     model: function() {
         return [
-            '<a href="#" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="All About The Front End">Front End Matters</a>',
-            '<a href="#matters" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="What Is All This Modern Browser Talk About?">Modern Browsers</a>',
+            '<a href="#matters" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="All About The Front End">Front End Matters</a>',
+            '<a href="#browsers" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="What Is All This Modern Browser Talk About?">Modern Browsers</a>',
             '<a href="#data" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Infographics And Charts Have Taken Over!">Big Data Visualization</a>',
             '<a href="#mobile" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="You Mean There Is Another Device Out There Besides My iPad?">Mobile First Design</a>',
             '<a href="#mvc" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Backend...You Sir Have Met Your Match!">Front End MVC</a>',
--- app/scripts/views/application_view.js
+++ app/scripts/views/application_view.js
@@ -1,4 +1,4 @@
-Core.ApplicationView = Ember.View.extend({
+Cor.ApplicationView = Ember.View.extend({
     didInsertElement: function(window, document) {
 
         var panelTip = ('.generated-left-panel');
@@ -39,5 +39,49 @@ Core.ApplicationView = Ember.View.extend({
             maxZoom: 14,
             minZoom: 09
         }).addTo(map);
+
+        //stick in the fixed 100% height behind the navbar but don't wrap it
+        jQuery('#slide-nav.navbar .container').append(jQuery('<div id="navbar-height-col"></div>'));
+
+        // Enter your ids or classes
+        var toggler = '.navbar-toggle';
+        var pagewrapper = '#main';
+        var navigationwrapper = '.navbar-header';
+        var menuwidth = '100%'; // the menu inside the slide menu itself
+        var slidewidth = '40%';
+        var menuneg = '-100%';
+        var slideneg = '-80%';
+
+        jQuery("#slide-nav").on("click", toggler, function(e) {
+            var selected = jQuery(this).hasClass('slide-active');
+
+            jQuery('#slidemenu').stop().animate({
+                left: selected ? menuneg : '0px'
+            });
+
+            jQuery('#navbar-height-col').stop().animate({
+                left: selected ? slideneg : '0px'
+            });
+
+            jQuery(pagewrapper).stop().animate({
+                left: selected ? '0px' : slidewidth
+            });
+
+            jQuery(navigationwrapper).stop().animate({
+                left: selected ? '0px' : slidewidth
+            });
+
+            jQuery(this).toggleClass('slide-active', !selected);
+            jQuery('#slidemenu').toggleClass('slide-active');
+            jQuery('#main, .navbar, body, .navbar-header').toggleClass('slide-active');
+        });
+
+        var selected = '#slidemenu, #main, body, .navbar, .navbar-header';
+        jQuery(window).on("resize", function() {
+
+            if (jQuery(window).width() > 767 && jQuery('.navbar-toggle').is(':hidden')) {
+                jQuery(selected).removeClass('slide-active');
+            }
+        });
     }
 });
