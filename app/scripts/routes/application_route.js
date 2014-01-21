0f9ae93ea6fb777fea195c291063a293935d95ff
Ninja <jvrunion@gmail.com>
2014-01-20 23:48:34 -0500

    made pre code block full width

--- app/scripts/routes/application_route.js
+++ app/scripts/routes/application_route.js
@@ -3,7 +3,7 @@ Cor.ApplicationRoute = Ember.Route.extend({
         return [
             '<a href="#matters" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="All About The Front End">Front End Matters</a>',
             '<a href="#browsers" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="What Is All This Modern Browser Talk About?">Modern Browsers</a>',
-            '<a href="#data" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Infographics And Charts Have Taken Over!">Big Data Visualization</a>',
+            '<a href="#data" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Infographics And Charts Have Taken Over!">Data Visualization</a>',
             '<a href="#mobile" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="You Mean There Is Another Device Out There Besides My iPad?">Mobile First Design</a>',
             '<a href="#mvc" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Backend...You Sir Have Met Your Match!">Front End MVC</a>',
             '<a href="#wcag" class="generated-left-panel" data-toggle="tooltip" data-placement="right" title="" data-original-title="Standards Are What Seperate The Successfull From The Failures">(WCAG) 2.0</a>'
